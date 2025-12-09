d01db10d2f467dd2f623f9941014a894f7ec56f1
Md Abdullah Al Maruf <maruf.diucse62@gmail.com>
2025-12-09 23:47:29 +0600

    Update model_download.py

--- model_download.py
+++ model_download.py
@@ -2,7 +2,7 @@ import gdown
 import os
 
 def download_model():
-    model_path = "best_model.pth"
+    model_path = "best_vegetable_model.pth"
 
     if not os.path.exists(model_path):
         url = "https://drive.google.com/uc?id=1OwWgl_R5Ff8vxyqjQ7JXKtL4l8C5jvmy"
